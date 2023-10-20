6711fde40a55cecba1f0c232e06cd3382450abec
EntoZz02 <145663506+EntoZz02@users.noreply.github.com>
2023-10-20 17:04:31 +0300

    Add files via upload

---
+++ Library recods/library records.sql
@@ -0,0 +1,9 @@
+CREATE TABLE library_records (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    adm_no VARCHAR(50) NOT NULL,
+    book_title VARCHAR(255) NOT NULL,
+    book_number VARCHAR(20) NOT NULL,
+    author VARCHAR(100) NOT NULL
+);
+
